3d6b5988e4b63d2b68a57a395b5f0a164c2bc233
dimariaw <dimariaw@foxmail.com>
2022-08-18 17:23:51 +0800

    football hard

--- tests/football/config.py
+++ tests/football/config.py
@@ -1,8 +1,7 @@
-NAME = "kaggle_truncated_cnn_impala"  # env_model_algorithm_other
+NAME = "kaggle_hard_cnn_impala"  # env_model_algorithm_other
 USE_BZ2 = True
 
 MEMORY_ADDRESS = ("10.255.85.17", 57779)
-#MEMORY2_ADDRESS = ("10.127.45.22", 57789)
 MODEL_ADDRESS = ("10.255.85.17", 57789)
 LEAGUE_ADDRESS = ("10.255.85.17", 57799)
 
