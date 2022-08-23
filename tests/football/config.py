9bfe9c67fa47ab3e47de0f59e293af1e6216ae6a
dimariaw <dimariaw@foxmail.com>
2022-08-23 21:02:25 +0800

    kaggle

--- tests/football/config.py
+++ tests/football/config.py
@@ -1,4 +1,4 @@
-NAME = "google_easy_feature_customized_checkpoints_impala6"  # env_model_algorithm_other
+NAME = "kaggle_easy_feature_impala"  # env_model_algorithm_other
 USE_BZ2 = True
 
 MEMORY_ADDRESS = ("10.255.85.17", 57779)
