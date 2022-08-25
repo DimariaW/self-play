95f8c32e43f31064256048f9afeffa8dfd33019e
dimariaw <dimariaw@foxmail.com>
2022-08-26 01:44:09 +0800

    all

--- tests/football/actor.py
+++ tests/football/actor.py
@@ -26,7 +26,7 @@ class ActorMain(core.ActorMainBase):
         # env = OpponentWrapper(env, opponents_pool)
 
     def create_env_and_agent(self, gather_id: int = None, actor_id: int = None):
-        env = FeatureEnv(reward_type="scoring")
+        env = FeatureEnv(reward_type="checkpoints")
         device = torch.device("cpu")
         model = FeatureModel().to(device)
         agent = IMPALAAgent(model, device=device)
--- tests/football/learner.py
+++ tests/football/learner.py
@@ -54,12 +54,12 @@ class LearnerMain(core.LearnerMainBase):
         impala = alg.IMPALA(model,
                             queue_senders,
                             tensor_receiver,
-                            # lr=0.00019896, gamma=0.993, lbd=1, vf=0.5 * 2, ef=0.00087453 * 2,
+                            lr=0.00019896, gamma=0.993, lbd=1, vf=0.5, ef=0.00087453,
                             # lr=0.0001, gamma=0.993, lbd=0.96, vf=1, ef=0.002,
-                            lr=0.0001, gamma=0.993, lbd=1, vf=1, ef=0.0002,
+                            # lr=0.0001, gamma=0.993, lbd=1, vf=1, ef=0.0002,
                             tensorboard_dir=os.path.join(self.logger_file_dir, "train_info"),
                             vtrace_key=["checkpoints"],
-                            upgo_key=["checkpoints"],
+                            # upgo_key=["checkpoints"],
                             )
         impala.run()
 
