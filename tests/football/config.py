d909af437f512854b3a6031518ab40273d1cb105
dimariaw <dimariaw@foxmail.com>
2022-08-29 20:53:01 +0800

    ip

--- tests/football/config.py
+++ tests/football/config.py
@@ -2,7 +2,7 @@
 NAME = "self_play_feature_checkpoints_impala"  # env_model_algorithm_other
 USE_BZ2 = True
 SELF_PLAY = True
-MEMORY_ADDRESS = ("10.255.85.17", 57779)
-MODEL_ADDRESS = ("10.255.85.17", 57789)
-LEAGUE_ADDRESS = ("10.255.85.17", 57799)
+MEMORY_ADDRESS = ("10.127.7.14", 57779)
+MODEL_ADDRESS = ("10.127.7.14", 57789)
+LEAGUE_ADDRESS = ("10.127.7.14", 57799)
 
